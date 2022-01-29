4fe1deb63beae7be7675332f690c85ffbfc594fc
jeromeOthot <jerome.othot@gmail.com>
2022-01-29 16:37:21 -0500

    Correction merge et  correction path input RPG_RT.ldb

--- Generateur/ZeldaSolarusRandomizer/Program.cs
+++ Generateur/ZeldaSolarusRandomizer/Program.cs
@@ -9,7 +9,7 @@ namespace ZeldaSolarusRandomizer
         static void Main(string[] args)
         {
            
-            string output = @"..\..\..\..\jeu zelda_mystery_of_solarus_randomizer\RPG_RT.ldb";
+            string rpgrtFile = @"..\..\..\..\jeu zelda_mystery_of_solarus_randomizer\RPG_RT.ldb";
             Randomizer randomizer = new Randomizer();
             
             bool success = false;
@@ -29,12 +29,12 @@ namespace ZeldaSolarusRandomizer
             FileManager fileManager = new FileManager();
             fileManager.BackupFileRPG_RT();
             
-            fileManager.ReadFile("RPG_RT.ldb");
+            fileManager.ReadFile(rpgrtFile);
             fileManager.GetStartChestIndex();
             fileManager.SetChestList(randomizer.ChestsList);
             fileManager.SetSeedByte(randomizer.Seed);
             fileManager.SetOptionByte((int)randomizer.Option);
-            fileManager.WriteOutputGame(output);
+            fileManager.WriteOutputGame(rpgrtFile);
 
             Console.WriteLine("Generation complete !!");
             Console.Read();  
